f0c84c886146e8210e34966569a0d3da904d5e35
Valhalla <dnapolitano5285@gmail.com>
2016-10-11 21:12:31 -0400

    Day 1 - base classes setup except for details page.
    API key removed - insert your own to query the moviedb

--- app/src/main/java/com/mediocremidgardian/popularmovies/QueryUtils.java
+++ app/src/main/java/com/mediocremidgardian/popularmovies/QueryUtils.java
@@ -10,7 +10,7 @@ public final class QueryUtils {
 
     private static final String MOVIEDB_POPULAR_URL = "https://api.themoviedb.org/3/movie/popular";
     private static final String MOVIEDB_TOP_RATES_URL = "https://api.themoviedb.org/3/movie/top_rated";
-    private static final String API_KEY ="b581ea78f98eaf9a41d6c35ec909b481";
+    private static final String API_KEY ="";
 
     private QueryUtils(){
 
